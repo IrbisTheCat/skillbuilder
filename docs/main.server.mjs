b6a87b333774657db32411539f5e0253b3bcd9fc
Anna Shchelokova <anna.shchelokova@gmail.com>
2025-01-25 15:44:30 -0500

    rebuild docs

--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-5RL4HO2M.mjs";import"./chunk-FNNV74EP.mjs";import"./chunk-Z4ES3KHK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2XHEKDPS.mjs";import"./chunk-OMLPOK4V.mjs";import"./chunk-UPZVVXI4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
