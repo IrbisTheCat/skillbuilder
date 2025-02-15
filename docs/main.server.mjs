335502397d7b4b05f7dcd9914afed75e872ed2d6
Anna Shchelokova <anna.shchelokova@gmail.com>
2025-02-14 21:56:10 -0500

    minor ux clean up

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IPXTP2RQ.mjs";import"./chunk-65LKIJUD.mjs";import"./chunk-CZWNZ2MR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-3NHTP5SY.mjs";import"./chunk-65LKIJUD.mjs";import"./chunk-CZWNZ2MR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
